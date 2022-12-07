a3ab7a5520f835c55a631b0871fce01bf567311b
Neimaad <damienlaoussing@hotmail.com>
2022-12-07 23:41:44 +0100

    update config

--- src/main/java/com/cedalanavi/projet_IJVA500_SOA_propositions/ProjetIJVA500SoaPropositionsApplication.java
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_propositions/ProjetIJVA500SoaPropositionsApplication.java
@@ -1,13 +1,13 @@
-package com.cedalanavi.projet_IJVA500_SOA_proprositions;
+package com.cedalanavi.projet_IJVA500_SOA_propositions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetIJVA500SoaProprositionsApplication {
+public class ProjetIJVA500SoaPropositionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetIJVA500SoaProprositionsApplication.class, args);
+		SpringApplication.run(ProjetIJVA500SoaPropositionsApplication.class, args);
 	}
 
 }
