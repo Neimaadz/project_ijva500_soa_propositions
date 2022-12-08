ff90b472cf80c4bdbf588c8517a3a799c7d45157
Neimaad <damienlaoussing@hotmail.com>
2022-12-08 01:00:05 +0100

    update name class

--- src/test/java/com/cedalanavi/projet_IJVA500_SOA_proprositions/ProjetIJVA500SoaPropositionsApplicationTests.java
+++ src/test/java/com/cedalanavi/projet_IJVA500_SOA_proprositions/ProjetIJVA500SoaPropositionsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetIJVA500SoaProprositionsApplicationTests {
+class ProjetIJVA500SoaPropositionsApplicationTests {
 
 	@Test
 	void contextLoads() {
